d9b0bb4008e91b8b82cf0e37aecfb71d8bb14781
28lmfc <137616926+28lmfc@users.noreply.github.com>
2026-06-26 11:03:38 +0100

    Add IsFinalized migration

---
+++ Migrations/20260626100337_AddIsFinalized.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AHM.Audit.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsFinalized : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
